4306951496ff40b90f0ae2687bad4283a5b03550
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-09-27 11:45:31 -0700

    Update toggle_rgb_state.py

--- class03/toggle_rgb_state.py
+++ class03/toggle_rgb_state.py
@@ -23,8 +23,7 @@ print('hexadecimal rgb_color =', hex(rgb_color))
 print('binary rgb_color =', bin(rgb_color))
 rgb2.fill_color(rgb_color)
 
-
-
+program_state = 'GREEN'
 
 # configure pin 8 as output:
 output_pin = Pin(8, mode=Pin.OUT)
@@ -35,6 +34,8 @@ input_pin_val = input_pin.value()  # read input pin value
 
 def output_toggle():
   global output_pin_val, output_pin
+  global program_state, rgb2
+  
   print('output toggle')
   output_pin_val = output_pin.value()  # read output value
   if output_pin_val == True: # if output pin is on (high or True)
@@ -45,10 +46,16 @@ def output_toggle():
    #output_pin.on()          # turn on output pin if input is False
    #output_pin.value(1)      # set output pin high
    output_pin.value(True)    # turn output pin high
+   
+  if program_state == 'GREEN':
+    program_state = 'YELLOW'
+    rgb2.fill_color(0xffff00)
+  
 
 
 while True:           # infinite loop
   M5.update()         # update M5 board
+  
   
   if input_pin_val == True:
     if input_pin.value() == False: # if the input pin is off (low or False)
