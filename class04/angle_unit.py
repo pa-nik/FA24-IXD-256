524be8f9244b03fc2d9ffd9995c5c7478683f466
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-10-04 11:39:46 -0700

    add remap to angle_val

--- class04/angle_unit.py
+++ class04/angle_unit.py
@@ -3,6 +3,7 @@ import M5
 from M5 import *
 from unit import AngleUnit
 import time
+import m5utils
 
 angle_0 = None
 angle_val = None
@@ -13,6 +14,8 @@ angle_0 = AngleUnit((1, 2))
 while True:
   M5.update()
   angle_val = angle_0.get_value()
+  # remap the range of values from 0 - 65335 to 0 - 255:
+  angle_val = int(m5utils.remap(angle_val, 0, 65535, 0, 255))
   print(angle_val)
   time.sleep_ms(500)
 
