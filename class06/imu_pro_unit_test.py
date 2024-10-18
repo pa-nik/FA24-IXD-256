bf44133e4d7f8a5628aa46968c6a54c8a12c5da0
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-10-18 11:06:53 -0700

    update imu examples

--- class06/imu_pro_unit_test.py
+++ class06/imu_pro_unit_test.py
@@ -18,9 +18,17 @@ i2c0 = I2C(0, scl=Pin(39), sda=Pin(38), freq=100000)
 # initialize IMU Pro unit:
 imupro_0 = IMUProUnit(i2c0)
 
-
 while True:
   M5.update()
+  
   imu_data = imupro_0.get_accelerometer()
-  print(imu_data)
+  #print(imu_data)
+  
+  acc_x = imu_data[0]  # X acceleration value
+  acc_y = imu_data[1]  # Y acceleration value
+  acc_z = imu_data[2]  # Z acceleration value
+  
+  # print acceleration values separated by comma:
+  print(acc_x, ',', acc_y, ',', acc_z)
+  
   time.sleep_ms(50)
--- class06/imu_test.py
+++ class06/imu_test.py
@@ -10,7 +10,16 @@ M5.begin()
 
 while True:
   M5.update()
+  
   imu_data = Imu.getAccel()
   print(imu_data)
+  
+  acc_x = imu_data[0]  # X acceleration value
+  acc_y = imu_data[1]  # Y acceleration value
+  acc_z = imu_data[2]  # Z acceleration value
+  
+  # print acceleration values separated by comma:
+  print(acc_x, ',', acc_y, ',', acc_z)
+  
   time.sleep_ms(50)
 
