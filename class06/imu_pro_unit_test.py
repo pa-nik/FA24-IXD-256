94ec52b6185b2c7a6e67366e07530ecbdd37f603
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-10-18 10:07:56 -0700

    Update imu_pro_unit_test.py

--- class06/imu_pro_unit_test.py
+++ class06/imu_pro_unit_test.py
@@ -8,8 +8,14 @@ import time
 print('imu example')
 
 M5.begin()
-i2c0 = I2C(0, scl=Pin(1), sda=Pin(2), freq=100000)
+# configure I2C on bottom connector of Atom Board:
+#i2c0 = I2C(0, scl=Pin(1), sda=Pin(2), freq=100000)
+# configure I2C on PortA (red connector):
+i2c0 = I2C(0, scl=Pin(39), sda=Pin(38), freq=100000)
+
 imupro_0 = IMUProUnit(i2c0)
+
+print('???')
 
 while True:
   M5.update()
