d697b36d982355a54125ec38d59de5d849073c44
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-11-01 09:55:03 -0700

    replace btnA_print

--- class08/protopie_test.py
+++ class08/protopie_test.py
@@ -8,9 +8,19 @@ import time
 # initialize M5 board:
 M5.begin()
 
+# configure analog to digital converter (ADC) on pin 1:
+adc = ADC(Pin(1), atten=ADC.ATTN_11DB)
+adc_val = None
+
 while True:           # infinite loop
   M5.update()         # update M5 board
+  
+  # read analog value from ADC:
+  adc_val = adc.read()
+  print(adc_val)
+  
   if BtnA.wasPressed():
       print('b')
+      
   time.sleep_ms(100)
     
