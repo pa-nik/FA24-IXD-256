82d417f5cc038569d2690325c547cdfc10a236da
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-11-15 12:14:08 -0800

    Create mqtt_button_publish_subscribe.py

---
+++ class10/mqtt_button_publish_subscribe.py
@@ -0,0 +1,65 @@
+# MQTT button publish to Adafruit IO feed
+
+import os, sys, io
+import M5
+from M5 import *
+from hardware import *
+import time
+import network
+from umqtt import *
+
+ssid = 'INSERT_WIFI_NAME'
+password = 'INSERT_WIFI_PASSWORD'
+
+mqtt_client = None
+aio_user_name = 'INSERT_ADAFRUIT_IO_USERNAME'
+aio_password = 'INSERT_ADAFRUIT_IO_PASSWORD'
+
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(ssid, password)
+
+print('connect to WiFi...')
+while wifi.isconnected() == False:
+    print('.', end='')
+    time.sleep_ms(100)
+
+print('WiFi connection successful')
+ip_list = wifi.ifconfig()
+ip_address = ip_list[0]
+print('IP address:', ip_address)
+
+mqtt_client = MQTTClient(
+  'testclient',
+  'io.adafruit.com',
+  port = 1883,
+  user = aio_user_name,
+  password = aio_password,
+  keepalive = 3000
+)
+mqtt_client.connect(clean_session=True)
+
+def feed_callback(data):
+  #print('received..', data)
+  print('received..', data[1].decode())
+
+mqtt_client.subscribe(aio_user_name+'/feeds/button-feed', feed_callback)
+
+M5.begin()
+
+while True:
+  M5.update()
+  
+  if BtnA.wasPressed():
+    print('button pressed..')
+    mqtt_client.publish(aio_user_name+'/feeds/button-feed', 'ON', qos=0)
+  elif BtnA.wasReleased():
+    print('button released..')
+    mqtt_client.publish(aio_user_name+'/feeds/button-feed', 'OFF', qos=0)
+
+  # check for messages from any subscribed feeds:
+  mqtt_client.check_msg()
+  
+  time.sleep_ms(100)  
+  
+
